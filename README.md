782390bfe27a890d4ce459a70b9a45ec9970d365
Rishab Ramanathan <rrishab2001@gmail.com>
2023-02-23 18:34:38 +0530

    code reorgnization, readme

---
+++ README.md
@@ -0,0 +1,39 @@
+# Text Detection and Recognition
+
+Rishab Ramanathan   
+19XJ1A0558
+
+A work in progress build to create and develop text-based augmented reality, with the goal of school textbook applications in mind. Ar this stage, the application can run text detection on a video stream.
+
+### System pre-requisites:
+- python3-tk (Tkinter - for GUI representation of matplotlib, used for the purpose of viewing the output dynamically instead of writing to a file)
+- tesseract-ocr (OCR library for character recognition. Use the following reference for installation : https://pyimagesearch.com/2017/07/03/installing-tesseract-for-ocr/)
+- pip3 (Python package manager)
+
+### Python pre-requisites:
+- pytesseract
+- opencv-contrib-python
+- imutils
+- matplotlib
+
+## Installation:
+1. (Optional) Set up the virtual environment using venv.
+> python3 -m venv <em>/path/to/virtualenv/target/directory</em>  
+> source <em>/path/to/virtualenv/target/directory/</em>bin/activate
+2. Install the necessary library dependencies
+> pip3 install -r requirements.txt
+3. (Subject to change) Navigate to text_recognition/ and run the test.py script
+> cd text_recognition/  
+> python3 test.py
+
+As of this moment, the script takes in an image/video input from a local assets/(images/videos) folder. I have consciously git ignored the videos folder to save storage space, so you are free to plug in your own image/video and modify the script to run the media. Just uncomment the specific function call and add in your address path as a parameter.  
+Future versions will void this in favour of command line arguments.
+
+## Test output :   
+### Image text detection :  
+### Video text detection :
+
+## TODO
+1. Improve video latency (currently skipping every 20th frame to improve performance)
+2. Explore more accurate text recognition engines
+3. Switch to live camera feed instead of a local video feed
--- text_recognition/test.py
+++ text_recognition/test.py
@@ -6,57 +6,65 @@ matplotlib.use('TkAgg', force=True)
 import matplotlib.pyplot as plt
 import re
 
-#img = cv2.imread('../assets/images/image1.jpg')
-#imgbox = pytesseract.image_to_boxes(img)
-#imgH, imgW, _ = img.shape
-#
-#for box in imgbox.splitlines():
-#    box = box.split(' ')
-#    x1 = int(box[1])
-#    y1 = int(box[2])
-#    x2 = int(box[3])
-#    y2 = int(box[4])
-#    ch = box[0]
-#    if(ch.isalnum()):
-#        print(f"{ch} - {ch.isalnum()}")
-#        cv2.rectangle(img, (x1,imgH - y1), (x2, imgH - y2), (255,0,0), 3)
+def imageTextDetection(imagePath='../assets/images/image1.jpg'):
+    img = cv2.imread(imagePath)
+    imgbox = pytesseract.image_to_boxes(img)
+    imgH, imgW, _ = img.shape
 
-font_scale = 1.5
-font = cv2.FONT_HERSHEY_SIMPLEX
-
-videocap = cv2.VideoCapture("../assets/videos/Inglourious_Basterds_Intro.mp4")
-
-if (not videocap.isOpened()):
-    cv2.VideoCapture(0)
-if (not videocap.isOpened()):
-    raise IOError ("Cannot open video")
-
-counter = 0
-while True:
-    ret,frame = videocap.read()
-    counter += 1
-    if((counter%20) == 0):
-
-        imgH, imgW,_ =frame.shape
-        x0,y0,w0,h0, = 0,0,imgH,imgW
-        imagestring = pytesseract.image_to_string(frame)
-        imgstr = re.sub(r'W+','',imagestring)
-        imageboxes = pytesseract.image_to_boxes(frame)
-
-        for box in imageboxes.splitlines():
-            box = box.split(' ')
-            x1,y1,w1,h1 = int(box[1]), int(box[2]), int(box[3]), int(box[4])
-            ch = box[0]
-            if(ch.isalnum()):
-                cv2.rectangle(frame, (x1, imgH- y1), (w1, imgH - h1), (255,0,0), 3)
-
-        cv2.putText(frame, imagestring, ((x0 + int(w0/25)), (y0 + int(h0/25))), font, 2, (0,0,255), 3)
-
-        cv2.imshow('balls',frame)
-        plt.show
-
-        if cv2.waitKey(2) & 0xFF == ord('q'):
-            break
+    for box in imgbox.splitlines():
+        box = box.split(' ')
+        x1 = int(box[1])
+        y1 = int(box[2])
+        x2 = int(box[3])
+        y2 = int(box[4])
+        ch = box[0]
+        if(ch.isalnum()):
+            print(ch)
+            cv2.rectangle(img, (x1,imgH - y1), (x2, imgH - y2), (255,0,0), 3)
     
-videocap.release()
-cv2.destroyAllWindows()
+    plt.imshow(img)
+    plt.show()
+
+def videoTextDetection(videoPath="../assets/videos/Inglourious_Basterds_Intro.mp4"):
+    font_scale = 1.5
+    font = cv2.FONT_HERSHEY_SIMPLEX
+
+    videocap = cv2.VideoCapture(videoPath)
+
+    if (not videocap.isOpened()):
+        cv2.VideoCapture(0)
+    if (not videocap.isOpened()):
+        raise IOError ("Cannot open video")
+
+    counter = 0
+    while True:
+        ret,frame = videocap.read()
+        counter += 1
+        if((counter%20) == 0):
+
+            imgH, imgW,_ =frame.shape
+            x0,y0,w0,h0, = 0,0,imgH,imgW
+            imagestring = pytesseract.image_to_string(frame)
+            imgstr = re.sub(r'W+','',imagestring)
+            imageboxes = pytesseract.image_to_boxes(frame)
+
+            for box in imageboxes.splitlines():
+                box = box.split(' ')
+                x1,y1,w1,h1 = int(box[1]), int(box[2]), int(box[3]), int(box[4])
+                ch = box[0]
+                if(ch.isalnum()):
+                    cv2.rectangle(frame, (x1, imgH- y1), (w1, imgH - h1), (255,0,0), 3)
+
+            cv2.putText(frame, imagestring, ((x0 + int(w0/25)), (y0 + int(h0/25))), font, 1, (0,0,255), 2)
+
+            cv2.imshow('balls',frame)
+            plt.show
+
+            if cv2.waitKey(2) & 0xFF == ord('q'):
+                break
+            
+    videocap.release()
+    cv2.destroyAllWindows()
+
+#imageTextDetection()
+#videoTextDetection()
